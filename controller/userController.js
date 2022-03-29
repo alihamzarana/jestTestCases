43f642f015237720a9b60b6057126ac666eee8d8
unknown <alirajpoot241@gmail.com>
2022-03-29 19:59:52 +0500

    add data to excel file

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,45 @@
 const User = require('../model/user');
+const User1 =  require('../db/User')
+const excelJS = require("exceljs");
+const exportUser = async (req, res) => {
+   const workbook = new excelJS.Workbook();  // Create a new workbook
+  const worksheet = workbook.addWorksheet("My Users"); // New Worksheet
+  const path = "./files";  // Path to download excel
+  // Column for data in excel. key must match data key
+  worksheet.columns = [
+    { header: "S no.", key: "s_no", width: 10 }, 
+    { header: "First Name", key: "fname", width: 10 },
+    { header: "Last Name", key: "lname", width: 10 },
+    { header: "Email Id", key: "email", width: 10 },
+    { header: "Gender", key: "gender", width: 10 },
+];
+// Looping through User data
+let counter = 1;
+User1.forEach((user) => {
+  user.s_no = counter;
+  worksheet.addRow(user); // Add data in worksheet
+  counter++;
+});
+// Making first line in excel bold
+worksheet.getRow(1).eachCell((cell) => {
+  cell.font = { bold: true };
+});
+try {
+  const data = await workbook.xlsx.writeFile(`${path}/users.xlsx`)
+   .then(() => {
+     res.send({
+       status: "success",
+       message: "file successfully downloaded",
+       path: `${path}/users.xlsx`,
+      });
+   });
+} catch (err) {
+    res.send({
+    status: "error",
+    message: err.message,
+  });
+  }
+};
 const handleSignUp = async (req, res) => {
 try {
       const user = await User.findOne({ email: req.body.email });
@@ -180,5 +221,6 @@ module.exports = {
  getAllUser,
  getUser,
  removeUser,
- updateUser
+ updateUser,
+ exportUser
 }
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { handleSignUp, getAllUser, getUser, removeUser,updateUser } = require('../controller/userController');
+const { handleSignUp, getAllUser, getUser, removeUser,updateUser, exportUser } = require('../controller/userController');
 router.post('/sign-up', handleSignUp);
 router.get('/', getAllUser);
+router.get("/downloadExcel", exportUser)
 router.get('/:id', getUser);
 router.put('/:id', updateUser )
 router.delete('/:id', removeUser )
